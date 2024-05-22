3b44beacaafc451e9d1d5ab3916ecf91413a84d9
William Ferreira <williamferreiragt@gmail.com>
2024-05-22 19:55:38 -0300

    Class 26: Uploading images

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -56,7 +56,7 @@ class PostsController extends Controller
             'title' => $request->title,
             'excerpt' => $request->excerpt,
             'body' => $request->body,
-            'image_path' => 'temporary',
+            'image_path' => $this->storeImage($request),
             'is_published' => $request->is_published === 'on',
             'min_to_read' => $request->min_to_read
         ]);
@@ -109,5 +109,12 @@ class PostsController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function storeImage($request)
+    {
+        $newImageName = uniqid() . '-' . $request->title . '.' . $request->image->extension();
+
+        return $request->image->move(public_path('images'), $newImageName);
     }
 }
