f3bd30a8c29065a517975e39f673455ad130ad46
William Ferreira <williamferreiragt@gmail.com>
2024-05-22 22:43:59 -0300

    Class 34: Many to many relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,4 +22,9 @@ class Post extends Model
     {
         return $this->hasOne(PostMeta::class);
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
